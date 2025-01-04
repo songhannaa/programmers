378ba6e7aeb6211b2cffe58ae05e9ada0eb6dbea
HAN NA <131218435+songhannaa@users.noreply.github.com>
2025-01-04 22:40:49 +0900

    [level 2] Title: 숫자의 표현, Time: 0.15 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    let count = 0;
+    for (let i=1; i*(i-1)/2<n; i++) {
+        if ((n-(i*(i-1))/ 2) % i === 0) {
+            count++;
+        }
+    }
+    return count;
+}
